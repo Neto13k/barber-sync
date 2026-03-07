acab1f9467c7082d87f147461145d3fd340229a4
José Hermes <josenetto13k@hotmail.com>
2026-03-06 22:29:38 -0300

    initial server api structure

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,7 @@ const axios = require('axios')
 
 app.use(express.json())
 
+app.get('/api/cadastro', (req, res) => {
+  console.log('Server funcionando'); 
+  res.json({ message: 'Server funcionando' });
+});
