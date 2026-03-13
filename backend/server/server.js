6c90aa3c4554640ad0a9e56ac1b3039d9cd68c38
José Hermes <josenetto13k@hotmail.com>
2026-03-13 20:55:28 -0300

    chore: organize backend architecture

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const axios = require('axios')
 
-app.use(express.json())
+express.use(express.json())
 
-app.get('/api/cadastro', (req, res) => {
+express.get('/api/cadastro', (req, res) => {
   console.log('Server funcionando'); 
   res.json({ message: 'Server funcionando' });
 });
