1f7e7c660a8e88d06ae3874cd233ec80cdd8a359
José Hermes <josenetto13k@hotmail.com>
2026-03-23 20:49:37 -0300

    feat: adiciona interceptors do Axios para gestão global de token JWT

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const api = axios.create({
   baseURL: "http://localhost:3000/",
@@ -7,3 +7,29 @@ export const api = axios.create({
   },
 
 });
+
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response; 
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.error('Não autorizado - Refaça o login');
+    }
+    return Promise.reject(error);
+  }
+);
