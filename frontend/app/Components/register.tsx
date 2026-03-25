683ba32f3262132137761e59fc67728dbbd6530e
José Hermes <josenetto13k@hotmail.com>
2026-03-24 21:34:45 -0300

    feat: implementa UX de registro com tratamento de erros, loading e redirecionamento

--- frontend/app/Components/register.tsx
+++ frontend/app/Components/register.tsx
@@ -1,8 +1,12 @@
-import { useForm } from "react-hook-form";
-import { Link } from "react-router";
+import { set, useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router";
 import { api } from "../services/api";
+import { useState } from "react";
 
 export function RegisterUser() {
+  const navigate = useNavigate();
+  const [registerError, setRegisterError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
   const {
     register,
     handleSubmit,
@@ -18,11 +22,17 @@ export function RegisterUser() {
   });
 
   const onSubmit = async (data: any) => {
+    setIsLoading(true);
+    setRegisterError(null);
     try {
       await api.post("/users", data);
+      navigate("/login");
       console.log("Dados enviados com sucesso:", data);
     } catch (error) {
       console.error("Erro ao enviar dados:", error);
+      setRegisterError("Erro ao registrar usuário. Por favor, tente novamente.");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -35,43 +45,40 @@ export function RegisterUser() {
           {...register("firstName", { required: "Nome é obrigatório" })}
           placeholder="Nome"
         />
+        {errors.firstName && <p>{errors.firstName.message}</p>}
 
         <input
           {...register("lastName", { minLength: 2 })}
           placeholder="Sobrenome"
         />
+        {errors.lastName && <p>{errors.lastName.message}</p>}
 
         <input
           type="email"
           placeholder="Digite seu e-mail"
           {...register("email", { required: "Email é obrigatório" })}
         />
-
-        {errors.email && <p>Preencha todos os campos</p>}
-
-        <br></br>
-        <br></br>
+        {errors.email && <p>{errors.email.message}</p>}
 
         <input
           type="password"
           placeholder="Digite sua senha"
           {...register("password", { required: "password é obrigatório" })}
         />
-
-        {errors.password && <p>Preencha todos os campos</p>}
-
-        <br></br>
-        <br></br>
-
+        {errors.password && <p>{errors.password.message}</p>}
+        
         <label>
           <input type="checkbox" {...register("isBarber")} />
           Sou Barbeiro
         </label>
 
-        <br></br>
+        {registerError && <p>{registerError}</p>}
+
         <br></br>
 
-        <button type="submit">Enviar</button>
+        <button type="submit" disabled = {isLoading}>
+          {isLoading ? "Finalizando cadastro..." : "Enviar"}
+        </button>
       </form>
 
       <br></br>
