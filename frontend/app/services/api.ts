987dacf0b42604e8187083c5b4ad721bb0e15755
José Hermes <josenetto13k@hotmail.com>
2026-03-29 22:04:46 -0300

    - feat(api): implementar logout automático em caso de erro 401

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -5,7 +5,6 @@ export const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-
 });
 
 
@@ -29,6 +28,9 @@ api.interceptors.response.use(
   (error) => {
     if (error.response && error.response.status === 401) {
       console.error('Não autorizado - Refaça o login');
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      window.location.href = '/login';
     }
     return Promise.reject(error);
   }
