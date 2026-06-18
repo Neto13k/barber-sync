6b1ea12b2e501679bb87ca720d4135125e4ec820
José Hermes <jose.hermes.dev@gmail.com>
2026-06-18 15:45:30 -0300

    feat: adiciona middleware de tratamento de erros no servidor

--- backend/server/server.js
+++ backend/server/server.js
@@ -7,11 +7,14 @@ const app = express();
 const cors = require('cors')
 const userRoutes = require('../routes/user.routes')
 const appointmentRoutes = require('../routes/appointment.routes')
+const errorHandler = require('../middleware/errorHandler');
 
 app.use(express.json())
 app.use(cors())
 app.use('/users', userRoutes);
 app.use('/appointments', appointmentRoutes);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
